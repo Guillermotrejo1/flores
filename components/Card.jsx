baf69e7c48a610bc92e58e263d297c27847e909e
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-12-10 21:45:54 -0600

    category section complete

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,14 +1,16 @@
 import Image from "next/image"
-import burger1 from "../public/img/burger-1.png"
+import Burger1 from "../public/img/burger-1.png"
+import Snack1 from "../public/img/snack-1.png"
+import Beverage2 from "../public/img/beverage-2.png"
 
 const Card = () => {
   return (
     <>
-      <div className="container flex flex-col gap-5">
-        <div className="bg-secondaryColor flex py-3 rounded-lg overflow-hidden">
+      <div className="container flex flex-col gap-5 md:flex-row">
+        <div className="bg-secondaryColor flex py-3 rounded-lg overflow-hidden md:flex-1">
           <div className="basis-1/3 relative">
             <Image
-              src={burger1}
+              src={Burger1}
               alt="Category Image"
               width={200}
               height={200}
@@ -22,6 +24,44 @@ const Card = () => {
               <p className="text-xs">Lorem ipsum dolor, sit amet</p>
             </div>
             <a href="#" className="text-blackColor cursor-pointer">Buy Online</a>
+          </div>
+        </div>
+        <div className="bg-redColor flex py-3 rounded-lg overflow-hidden md:flex-1">
+          <div className="basis-1/3 relative">
+            <Image
+              src={Snack1}
+              alt="Category Image"
+              width={200}
+              height={200}
+              className="absolute w-28 -bottom-4 -left-4"
+            />
+          </div>
+
+          <div>
+            <div className="mb-2">
+              <h4 className="card__title">Snack</h4>
+              <p className="text-xs">Lorem ipsum dolor, sit amet</p>
+            </div>
+            <a href="#" className="text-secondaryColor cursor-pointer">Buy Online</a>
+          </div>
+        </div>
+        <div className="bg-greenColor flex py-3 rounded-lg overflow-hidden md:flex-1">
+          <div className="basis-1/3 relative">
+            <Image
+              src={Beverage2}
+              alt="Category Image"
+              width={200}
+              height={200}
+              className="absolute w-28 -bottom-4 -left-4"
+            />
+          </div>
+
+          <div>
+            <div className="mb-2">
+              <h4 className="card__title">Beverage</h4>
+              <p className="text-xs">Lorem ipsum dolor, sit amet</p>
+            </div>
+            <a href="#" className="text-secondaryColor cursor-pointer">Buy Online</a>
           </div>
         </div>
       </div>
